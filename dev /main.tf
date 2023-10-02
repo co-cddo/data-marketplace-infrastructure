71505b87b7992c7a05c2fe4e91708df565b10bab
CrinaID <117096090+CrinaID@users.noreply.github.com>
2023-10-02 11:40:37 +0100

    local-exec

--- dev /main.tf
+++ dev /main.tf
@@ -31,6 +31,12 @@ module "eks_cluster"{
     public_subnet_two_id = module.vpcmodule.public_subnets_output[1]
 
 }
+provisioner "local-exec" {
+    command =  <<EOH
+    aws eks update-kubeconfig --name ${var.cluster_name} --region ${var.region}
+    export KUBE_CONFIG_PATH=/home/ec2-user/.kube/config
+    EOH
+}
 module "load_balancer_dev" {
     source = "../modules/load-balancer-dev"
     vpc_id = module.vpcmodule.vpc.id
