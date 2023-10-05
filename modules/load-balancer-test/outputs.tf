5846d3b292c3d79dfbec9355047144dbaa10d302
soydaner.ulker <5871105+soydaner@users.noreply.github.com>
2023-10-05 18:23:17 +0000

    test

--- modules/load-balancer-test/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "openid_connector" {
-    value = aws_iam_openid_connect_provider.eks
-}
--- modules/load-balancer-test/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "eks_cluster" {}
-variable "vpc_id" {}
-variable "env_name" {}
-variable "eks_fargate_profile_kubesystem" {}
-variable "eks_fargate_profile_staging" {}
-variable "cluster_name" {}
-variable "user_name" {}
-variable "region" {}
