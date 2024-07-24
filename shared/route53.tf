a05d7a60edb57bab68a824d736de64d2e116800c
soydaner <5871105+soydaner@users.noreply.github.com>
2024-07-24 13:04:04 +0100

    Update route53.tf

--- shared/route53.tf
+++ shared/route53.tf
@@ -1,6 +1,6 @@
 ## Hosted zone
 variable "domain_name" {
-  default = "datamarketplace.gov.uk
+  default = "datamarketplace.gov.uk"
 }
 
 resource "aws_route53_zone" "datamarketplace" {
@@ -59,62 +59,49 @@ resource "aws_route53_record" "dev-txt" {
   ttl     = 300
   records = ["_p66t5uybni3hzxpa6hg4sct6iaowl2n"]
 }
-module "datamarketplace" {
-  source  = "terraform-aws-modules/route53/aws//modules/records"
-  version = "~> 2.0"
-  
 
-  zone_name = "datamarketplace.gov.uk"
+resource "aws_route53_record" "test-cname" {
+  zone_id = aws_route53_zone.datamarketplace.zone_id
+  name    = "test"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["cddo-test-f5g3gugzffgba3et.a02.azurefd.net"]
+}
+resource "aws_route53_record" "test-txt" {
+  zone_id = aws_route53_zone.datamarketplace.zone_id
+  name    = "_dnsauth.test"
+  type    = "TXT"
+  ttl     = 300
+  records = ["_2e9i57q84sztg6d6pptoozfmsmt6xgf"]
+}
 
-  records = [
-    {
-      name    = "test"
-      type    = "CNAME"
-      ttl     = 300
-      records = [
-        "cddo-test-f5g3gugzffgba3et.a02.azurefd.net",
-      ]
-    },
-    {
-      name    = "_dnsauth.test.datamarketplace.gov.uk"
-      type    = "TXT"
-      ttl     = 300
-      records = [
-        "_2e9i57q84sztg6d6pptoozfmsmt6xgf",
-      ]
-    },
-    {
-      name    = "preview"
-      type    = "CNAME"
-      ttl     = 300
-      records = [
-        "cddo-production-preview-gxhhc2cte4fpcnd2.a03.azurefd.net",
-      ]
-    },
-    {
-      name    = "_dnsauth.preview.datamarketplace.gov.uk"
-      type    = "TXT"
-      ttl     = 300
-      records = [
-        "_mmunqo9xxjel1981uwpbvre4ahw924l",
-      ]
-    },
-    {
-      name    = "www"
-      type    = "CNAME"
-      ttl     = 300
-      records = [
-        "cddo-production-dneqgkgqfnaxg2ds.a03.azurefd.net",
-      ]
-    },
-    {
-      name    = "_dnsauth.datamarketplace.gov.uk"
-      type    = "TXT"
-      ttl     = 300
-      records = [
-        "_nrkkr1fctcisn0s9fyj77gyqa2u392m",
-      ]
-    },
-  ]
+resource "aws_route53_record" "prev-cname" {
+  zone_id = aws_route53_zone.datamarketplace.zone_id
+  name    = "preview"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["cddo-production-preview-gxhhc2cte4fpcnd2.a03.azurefd.net"]
+}
+resource "aws_route53_record" "prev-txt" {
+  zone_id = aws_route53_zone.datamarketplace.zone_id
+  name    = "_dnsauth.preview"
+  type    = "TXT"
+  ttl     = 300
+  records = ["_mmunqo9xxjel1981uwpbvre4ahw924l"]
+}
+
+resource "aws_route53_record" "prod-cname" {
+  zone_id = aws_route53_zone.datamarketplace.zone_id
+  name    = "www"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["ddo-production-dneqgkgqfnaxg2ds.a03.azurefd.net"]
+}
+resource "aws_route53_record" "prod-txt" {
+  zone_id = aws_route53_zone.datamarketplace.zone_id
+  name    = "_dnsauth.www"
+  type    = "TXT"
+  ttl     = 300
+  records = ["_nrkkr1fctcisn0s9fyj77gyqa2u392m"]
 }
 
