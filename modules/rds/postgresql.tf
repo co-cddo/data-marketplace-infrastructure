a7903633b44899b21a289b77a5ffdbf8b79b37a7
John Palmer <john.palmer1@bp.com>
2025-05-06 16:08:05 +0100

    Cleanup: removed commented redundant code at modules/rds/postgresql.tf

--- modules/rds/postgresql.tf
+++ modules/rds/postgresql.tf
@@ -1,13 +1,3 @@
-#data "aws_secretsmanager_secret" "db_password" {
-#  name = "dm-gen-mssql-${var.env_name}-masterpassword"
-#}
-#
-#data "aws_secretsmanager_secret_version" "db_password" {
-#  secret_id = data.aws_secretsmanager_secret.db_password.id
-#}
-
-
-
 resource "aws_db_instance" "postgresql_instance" {
   identifier        = "${var.project_code}-${var.env_name}-rds-postgresql-instance"
   engine            = "postgres"
@@ -32,37 +22,3 @@ resource "aws_db_instance" "postgresql_instance" {
     Name = "${var.project_code}-${var.env_name}-eks-sg"
   }
 }
-
-#resource "aws_db_subnet_group" "db_subnet_group" {
-#  name       = "${var.project_code}-${var.env_name}-mssql-subnet-group"
-#  subnet_ids = [var.private_subnet_one_id, var.private_subnet_two_id]
-#
-#  tags = {
-#    Name = "POSTGRESQL DB Subnet Group"
-#  }
-#}
-#
-#resource "aws_security_group" "db_sg" {
-#  name        = "${var.project_code}-${var.env_name}-mssql-sg"
-#  description = "Security group for POSTGREQL RDS instance"
-#  vpc_id      = var.eks_vpc_id
-#
-#  # Add ingress rules as needed
-#  ingress {
-#    from_port   = 5432
-#    to_port     = 5432
-#    protocol    = "tcp"
-#    cidr_blocks = [var.vpc_cidr]
-#  }
-#
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  tags = {
-#    Name = "POSTGRESQL Security Group"
-#  }
-#}
