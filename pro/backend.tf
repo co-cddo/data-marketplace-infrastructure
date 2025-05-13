3fcbdf72f0ee3ebd7b1b3ca302d2de7ae07b332f
soydaner.ulker <5871105+soydaner@users.noreply.github.com>
2025-05-13 13:13:36 +0000

    prod env backend s3 bucket

--- pro/backend.tf
+++ pro/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dm-gen-config"
+    bucket       = "dm-gen-config-prod"
     key          = "pro/terraform.tfstate"
     region       = "eu-west-2"
     use_lockfile = true
