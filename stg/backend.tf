49b1eaac5310dd25c8679d4db300b2bafba44620
soydaner.ulker <5871105+soydaner@users.noreply.github.com>
2025-05-17 06:56:44 +0000

    stage backend fixed

--- stg/backend.tf
+++ stg/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # BUCKET NAME SHOULD BE SET ACCORDING TO ACCOUNT TYPE
-    bucket       = "dm-gen-config"
+    bucket       = "dm-gen-config-prod"
     key          = "terraform/stg/terraform.tfstate"
     region       = "eu-west-2"
     use_lockfile = true
