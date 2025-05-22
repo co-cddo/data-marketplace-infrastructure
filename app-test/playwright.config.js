532324f57c3883f02bdce463d827d9800c35df8d
Denis Davidov <denislav.davidov@digital.cabinet-office.gov.uk>
2025-05-22 10:14:12 +0100

    update config file

--- app-test/playwright.config.js
+++ app-test/playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 1000,
+  timeout: 60000,
   retries: 0,
   use: {
     headless: true,
