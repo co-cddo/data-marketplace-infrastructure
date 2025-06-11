18d7b8d1ac8bd04096fb2ad13be5c3115d773322
John Palmer <john.palmer@digital.cabinet-office.gov.uk>
2025-06-11 17:45:21 +0100

    Commented out owerwrite entry

--- modules/parameter-store/main.tf
+++ modules/parameter-store/main.tf
@@ -40,7 +40,7 @@ resource "aws_ssm_parameter" "stringlist_parameters" {
   value     = local.stringlist_initial_value
   tier      = local.stringlist_tier
   key_id    = local.key_id
-  overwrite = local.overwrite
+ #overwrite = true
   lifecycle {
     ignore_changes = [
       value,
