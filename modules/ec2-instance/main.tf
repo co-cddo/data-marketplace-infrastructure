21c0ff474dd1191ff90c3c4e1748adc405b6d1a2
John Palmer <john.palmer@digital.cabinet-office.gov.uk>
2025-06-11 23:47:22 +0100

    Declared Assume Role for Instance

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -1,24 +1,24 @@
-#resource "aws_iam_role" "ssm_role" {
-#  name = "SSMRole_jp_sandbox"
-#
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17",
-#    Statement = [
-#      {
-#        Effect = "Allow",
-#        Principal = {
-#          Service = "ec2.amazonaws.com"
-#        },
-#        Action = "sts:AssumeRole"
-#      }
-#    ]
-#  })
-#}
-#
-#resource "aws_iam_role_policy_attachment" "ssm_attach" {
-#  role       = aws_iam_role.ssm_role.name
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-#}
+resource "aws_iam_role" "ssm_role" {
+  name = "SSMInstanceRoleForAssume"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
 
 resource "aws_iam_instance_profile" "ssm_profile" {
   name = "SSMInstanceProfile"
