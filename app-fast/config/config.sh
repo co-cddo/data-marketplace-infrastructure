04dd4bb2029c8c9a454c88b5fe8c17f324ee69ba
John Palmer <john.palmer@digital.cabinet-office.gov.uk>
2025-07-16 13:10:31 +0100

    Initial Commit

--- app-fast/config/config.sh
+++ app-fast/config/config.sh
@@ -31,9 +31,9 @@ if [ "$BASH_MAJOR_VERSION" -lt 4 ]; then
     exit 1
 fi
 
-_ENVNAME=${1}
+ENVNAME=${1}
 
-if [ -z "$_ENVNAME" ]; then
+if [ -z "$ENVNAME" ]; then
     echo "ERROR: No environment name provided. Usage: $0 <environment_name>"
     exit 1
 fi
@@ -42,91 +42,7 @@ echo "--------------------------------------------"
 echo "--- TOPDIR:        $TOPDIR"
 echo "--- BASH VERSION:  $BASH_VERSION"
 echo "--- BASH PATH:     $BASH_PATH"
+echo "---"
+echo "--- ENVNAME:       $ENVNAME"
 echo "--------------------------------------------"
 
-_ENVIRONMENTS=(${1})
-_DATAFILENAME=vardata
-
-for _ENV in "${_ENVIRONMENTS[@]}"; do
-    echo "--------------------------------------------"
-    echo "--- ENVIRONMENT:  $_ENV"
-    echo "--------------------------------------------"
-
-    _TEMPLATEDIR=${TOPDIR}/config/templates
-    _OUTPUTDIR=${TOPDIR}/config/output
-    _DATAFILEDIR=${TOPDIR}/config/inputdata
-    _DATAFILECOMBINEDNAME=${_ENV}-${_DATAFILENAME}
-    _DATAFILE=${_DATAFILEDIR}/${_DATAFILECOMBINEDNAME}
-
-    mkdir -p  ${_TEMPLATEDIR} ${_OUTPUTDIR} ${_DATAFILEDIR}
-
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    echo "~~ CLEANING UP Leftovers From Previous Runs (if any)"
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    rm -f ${_DATAFILE}
-
-    echo "--- INFO: Getting Master Data File for Environment: ${_ENV}"
-
-    aws ssm get-parameters \
-    --name "/dm/${_ENV}/config-inputs" \
-    --with-decryption \
-    --query "Parameters[*].{Value:Value}" \
-    --output text > ${_DATAFILE}
-
-    EXIT_STATUS=$?
-    if [ "$EXIT_STATUS" -ne "0" ]
-    then
-        echo "ERROR: Something went wrong with the aws command"
-        echo
-        continue
-    fi
-
-    _TEMPLATEFILES=$(ls -1 ${_TEMPLATEDIR}/)
-    _TFILEARRAY=($_TEMPLATEFILES)
-    for _TFILE in "${_TFILEARRAY[@]}"; do
-
-        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        echo "~~ TFILE:  $_TFILE"
-        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        source ${_DATAFILE}
-        cfgtemplate="${_TEMPLATEDIR}/${_TFILE}"
-        outputfile="${_OUTPUTDIR}/${_ENV}-${_TFILE}"
-        tmpfile=$(mktemp)
-        $CP --attributes-only --preserve $cfgtemplate $tmpfile
-        cat $cfgtemplate | ${ENVSUBST} > $tmpfile && mv $tmpfile $outputfile
-        echo "------------------------"
-        echo "--- RESULT:  output/$(basename $outputfile)"
-        echo "------------------------"
-        cat $outputfile
-
-        _IDTAG=`echo ${_TFILE} | awk -F\- '{print $1}'`
-        _UPLDFILENAME="/dm/${_ENV}/appsettings/${_IDTAG}"
-
-        echo "--------------------------------------------"
-        echo "--- TFILE          :  $_TFILE"
-        echo "--- IDTAG          :  ${_IDTAG}"
-        echo "--- UPLOADING FROM :  output/$(basename $outputfile)"
-        echo "--- UPLOADING AS   :  ${_UPLDFILENAME}"
-        echo "--------------------------------------------"
-
-        echo "aws ssm put-parameter \\"
-        echo "--name "${_UPLDFILENAME}" \\"
-        echo "--type SecureString \\"
-        echo "--value file://${outputfile} \\"
-        echo "--overwrite"
-
-        aws ssm put-parameter \
-        --name "${_UPLDFILENAME}" \
-        --type SecureString \
-        --value file://${outputfile} \
-        --overwrite
-
-        echo
-        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        echo "~~ CLEANING UP"
-        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        rm -f ${outputfile}
-        rm -f ${tmpfile}
-    done
-    rm -f ${_DATAFILE}
-done
